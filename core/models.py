98af930ac12eb6b90d62c2da53d63b2768df7908
Wxrzd <tim.protze@gmail.com>
2025-03-13 09:29:29 +0100

    Füge Modelle für Nutzer, Vertrag und deren Beziehungen hinzu

--- core/models.py
+++ core/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+class Nutzer(models.Model):
+    Benutzername = models.CharField(max_length=100, primary_key=True, db_column='Benutzername')
+    Email = models.CharField(max_length=100, db_column='Email')
+    Passwort = models.CharField(max_length=100, db_column='Passwort')
+
 class Konto(models.Model):
     KontoNr = models.AutoField(primary_key=True)  
     Kontobezeichnung = models.CharField(max_length=100, db_column='Kontobezeichnung')
     Kontotyp = models.CharField(max_length=50, db_column='Kontotyp')
     Kontostand = models.DecimalField(max_digits=10, decimal_places=2, db_column='Kontostand')
-    Benutzername = models.CharField(max_length=100, db_column='Benutzername')
+    Benutzername = models.ForeignKey(Nutzer, on_delete=models.CASCADE, db_column='Benutzername')
 
     class Meta:
         db_table = 'Konto'  # Stellt sicher, dass Django die bestehende MySQL-Tabelle nutzt
@@ -13,19 +18,25 @@ class Konto(models.Model):
     def __str__(self):
         return self.Kontobezeichnung
 
-class Vertrag(models.Model):
-    class Meta:
-        db_table = "Vertrag"  # Setzt den exakten Tabellennamen in MySQL
-
-    VertragsNr = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
 class Kategorie(models.Model):
     class Meta:
         db_table = "Kategorie"  # Setzt den exakten Tabellennamen in MySQL
 
     KategorieNr = models.AutoField(primary_key=True)
     Kategoriebezeichnung = models.CharField(max_length=100, db_column='Kategoriebezeichnung')
+
+class Vertrag(models.Model):
+    class Meta:
+        db_table = "Vertrag"  # Setzt den exakten Tabellennamen in MySQL
+
+    VertragsNr = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100, db_column='Name')
+    Betrag = models.DecimalField(max_digits=10, decimal_places=2, db_column="Betrag")
+    Ablaufdatum = models.DateField(db_column="Ablaufdatum")
+    Intervall = models.CharField(max_length=50, db_column="Intervall")
+    Benutzernahme = models.ForeignKey(Nutzer, on_delete=models.CASCADE, db_column='Benutzername')
+    KontoNr = models.ForeignKey(Konto, on_delete=models.CASCADE, db_column='KontoNr')
+    KategorieNr = models.ForeignKey('Kategorie', on_delete=models.CASCADE, db_column='KategorieNr')
 
 class Buchung(models.Model):
     class Meta:
