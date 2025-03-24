719eae7f4c1a2bf2a75251ee9cf1061a8d77752f
Wxrzd <github@wxrzd.de>
2025-03-24 11:17:01 +0100

    Füge gruppierte Kategorienauswahl zu Formularen hinzu: implementiere Funktion zur Auswahl von Kategorien basierend auf Hauptkategorien; aktualisiere die Kategorie-Felder in Buchungs- und Budgetformularen

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,60 @@
 from django import forms
 from datetime import date
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
+from django import forms
+from itertools import groupby
+from operator import attrgetter
 from .models import Buchung, Benutzer, Konto, Vertrag, Kategorie, Budget
 from django.utils.timezone import now
+
+def get_kategorie_choices(user):
+    from .models import Hauptkategorie, Kategorie
+    from django.db.models import Q
+
+    hauptkategorien = Hauptkategorie.objects.all()
+    kategorien = Kategorie.objects.filter(Q(benutzer=user) | Q(benutzer=None)).select_related('hauptkategorie')
+
+    grouped_choices = []
+    for haupt in hauptkategorien:
+        unterkats = kategorien.filter(hauptkategorie=haupt)
+        if unterkats.exists():
+            grouped_choices.append((
+                haupt.name,
+                [(kat.pk, kat.kategoriebezeichnung) for kat in unterkats]
+            ))
+    return grouped_choices
+
+class GroupedModelMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def __init__(self, group_by_field, group_label=None, *args, **kwargs):
+        self.group_by_field = group_by_field
+        self.group_label = group_label or (lambda group: str(group))
+        super().__init__(*args, **kwargs)
+
+    def _get_choices(self):
+        if hasattr(self, '_choices'):
+            return self._choices
+        return self._build_grouped_choices()
+    
+    @property
+    def choices(self):
+        return self._build_grouped_choices()
+
+    @choices.setter
+    def choices(self, value):
+        self._choices = value
+
+    def _build_grouped_choices(self):
+        if self.queryset is None:
+            return []
+        choices = []
+        queryset = list(self.queryset.all())
+        queryset.sort(key=lambda obj: getattr(obj, self.group_by_field).name)
+        for group, items in groupby(queryset, key=attrgetter(self.group_by_field)):
+            group_label = self.group_label(group)
+            group_choices = [(self.prepare_value(item), self.label_from_instance(item)) for item in items]
+            choices.append((group_label, group_choices))
+        return choices
 
 class RegistrierungsForm(UserCreationForm):
     class Meta:
@@ -49,7 +101,7 @@ class BuchungForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if user:
             self.fields['konto'].queryset = Konto.objects.filter(benutzer=user)
-            self.fields['kategorie'].queryset = Kategorie.objects.filter(benutzer=user)
+            self.fields['kategorie'].choices = get_kategorie_choices(user)
             self.fields['vertrag'].queryset = Vertrag.objects.filter(benutzer=user)
 
 class BuchungEinnahmeForm(forms.ModelForm):
@@ -85,7 +137,7 @@ class BuchungEinnahmeForm(forms.ModelForm):
         self.instance.buchungsart = 'Einnahme'
         if user:
             self.fields['konto'].queryset = Konto.objects.filter(benutzer=user)
-            self.fields['kategorie'].queryset = Kategorie.objects.filter(benutzer=user)
+            self.fields['kategorie'].choices = get_kategorie_choices(user)
             self.fields['vertrag'].queryset = Vertrag.objects.filter(benutzer=user)
 
 class BuchungAusgabeForm(forms.ModelForm):
@@ -121,7 +173,7 @@ class BuchungAusgabeForm(forms.ModelForm):
         self.instance.buchungsart = 'Ausgabe'
         if user:
             self.fields['konto'].queryset = Konto.objects.filter(benutzer=user)
-            self.fields['kategorie'].queryset = Kategorie.objects.filter(benutzer=user)
+            self.fields['kategorie'].choices = get_kategorie_choices(user)
             self.fields['vertrag'].queryset = Vertrag.objects.filter(benutzer=user)
 
 class KontoForm(forms.ModelForm):
@@ -165,7 +217,7 @@ class VertragForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if user:
             self.fields['konto'].queryset = Konto.objects.filter(benutzer=user)
-            self.fields['kategorie'].queryset = Kategorie.objects.filter(benutzer=user)
+            self.fields['kategorie'].choices = get_kategorie_choices(user)
 
 class KategorieForm(forms.ModelForm):
     class Meta:
@@ -178,15 +230,20 @@ class KategorieForm(forms.ModelForm):
             self.instance.benutzer = user
 
 class BudgetForm(forms.ModelForm):
+    kategorien = forms.ModelMultipleChoiceField(
+        queryset=Kategorie.objects.none(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
     class Meta:
         model = Budget
         fields = ['name', 'betrag', 'kategorien']
-        widgets = {
-            'kategorien': forms.CheckboxSelectMultiple(),
-        }
 
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
         if user:
-            self.fields['kategorien'].queryset = Kategorie.objects.filter(benutzer=user)
+            self.fields['kategorien'].queryset = Kategorie.objects.filter(
+                Q(benutzer=user) | Q(benutzer=None)
+            ).select_related('hauptkategorie').order_by('hauptkategorie__name', 'kategoriebezeichnung')
             self.instance.benutzer = user
+
